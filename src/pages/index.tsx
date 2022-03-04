f00a51c34cbdfa29b0c0534195739d08eebbef77
Paolo Viselli <pviselli@dutchdigital.systems>
2022-03-04 17:07:45 +0000

    commented buggy import pt 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,3 +7,37 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+// The below is to be pasted in an auth.tsx file localted in the same folder as this file
+
+// import React from "react";
+// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+// import firebase from "../../firebase/clientApp";
+
+// // Configure FirebaseUI.
+// const uiConfig = {
+//   // Redirect to / after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+//   signInSuccessUrl: "/",
+//   // We will display GitHub as auth providers.
+//   signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID],
+// };
+
+// function SignInScreen() {
+//   return (
+//     <div
+//       style={{
+//         maxWidth: "320px",
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         justifyContent: "center",
+//       }}
+//     >
+//       <h1>Welcome to Amazon</h1>
+//       <p>Please sign-in:</p>
+//       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+//     </div>
+//   );
+// }
+
+// export default SignInScreen;
